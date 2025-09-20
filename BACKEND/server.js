c8cdb5239667b9a68f254fda235abb04db6156fa
Samuel Maina <135238870+samde96@users.noreply.github.com>
2025-09-20 19:27:32 +0300

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -33,7 +33,7 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("Connected to MongoDB");
         app.listen(5000, () => {
-            console.log('Server is running on Port 5000!');
+            console.log('Server is running on 'PORT!');
         });
     })
     .catch((err) => {
